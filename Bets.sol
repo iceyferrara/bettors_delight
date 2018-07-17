d63bd2f19a7c401012bc88295efde921ce44944d
iceyferrara <ferrara18@gmail.com>
2018-07-17 16:34:39 -0400

    Update Bets.sol
    Place inside :/Betting/Contracts/

--- Bets.sol
+++ Bets.sol
@@ -1,11 +1,11 @@
 pragma solidity 0.4.24;
 
 contract Bets {
-
+//Info from HLTV for team will go in this struct
     struct Team {
-      uint id;
-      string name;
-      uint betCount;
+      uint id;   //match id?
+      string name; //team name
+      uint betCount; //Amount?
     }
 // Read/write candidate
   mapping(uint => Team) public teams;
