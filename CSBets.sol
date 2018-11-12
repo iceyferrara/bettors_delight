65a921c31862aa3e85c032f531265dbd3f247200
Chris Bergamasco <40219354+CMasco@users.noreply.github.com>
2018-11-12 12:26:04 -0500

    Add files via upload

--- CSBets.sol
+++ CSBets.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.20;
 
 
 //Chris Bergamasco
@@ -13,7 +13,6 @@ contract CSBets is usingOraclize {
   //model of a Match
   struct Match{
     uint id; //id of match
-    string theWinner; //String for Json data to pick winner of each specific match
     string team1;
     string team2;
     uint256 t1_pool; //pool for team1 bets
@@ -35,36 +34,33 @@ contract CSBets is usingOraclize {
     bool rewarded; //flag for doublespend
     mapping(bytes32 => uint) bets; //array of bets
   }
+/*
+  struct InputToResult{
+    string result;
+  }
 
-
-struct oraclizeCallback {
-    // for which purpose did we call? {ForUSD | ForDistance}
-    oraclizeState oState;
-}
+  struct QueryIDtoQuery{
+    string id;
+  } */
 
   //Store accounts that have placed bets. Used to make sure
   //that users dont call CalculateResults more than once
   mapping(address => bool) public bettors;
 
-
   mapping(uint => Match) public matches;
   mapping(address => Bet) public bets;
   mapping(uint => bets_info) public payoutIndex;
-  mapping(bytes32 => oraclizeCallback) public oraclizeCallbacks;
+  mapping(uint => string) public InputToResult;
+  mapping(bytes32 => uint) internal QueryIDtoMatchID;
 
   uint matchCount;
 
   string public jsonData;
   bytes32 oraclizeID;
-//  string public testString = "json(https://api.pandascore.co/dota2/matches.json?filter[id]=52365&token=tU9uGM46ds_tXnE6FkW3u9g43EV1HsfuXOBPVNkmPHOBzMDK13Q).0.winner.name";
-  string public firstHalfQuery ="json(https://api.pandascore.co/dota2/matches.json?filter[id]=";
-  string public secondHalfQuery ="&token=tU9uGM46ds_tXnE6FkW3u9g43EV1HsfuXOBPVNkmPHOBzMDK13Q).0.winner.name";
 
-  //Function for distance retrieval
-  function distanceRetrieve(uint _matchID) payable {
-        bytes32 queryId = oraclize_query("URL",  strConcat(firstHalfQuery, uint2str(_matchID), secondHalfQuery));
-        oraclizeCallbacks[queryId] = oraclizeCallback(oraclizeState._matchID);
-  }
+  string public testString = "json(https://api.pandascore.co/dota2/matches.json?filter[id]=52365&token=tU9uGM46ds_tXnE6FkW3u9g43EV1HsfuXOBPVNkmPHOBzMDK13Q).0.winner.name";
+  string public firstHalfQuery ="json(https://api.pandascore.co/dota2/matches.json?filter[id]=";
+  string public secondHalfQuery ="&token=wzNmqc4OVd4S03FTIJmFFRENM6GaHh4SQh3ZKQNr-JS5RWxAYx4).0.winner.name";
 
   //constructor
   constructor() public payable {
@@ -73,112 +69,114 @@ struct oraclizeCallback {
     oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS);
   }
 
-  function fetchMatchInfo(uint _matchID) onlyOwner payable{
-    oraclizeID = oraclize_query("URL",  strConcat(firstHalfQuery, uint2str(_matchID), secondHalfQuery));
-  }
+  function fetchMatchResults(uint _matchID) payable onlyOwner {
+    string memory query = strConcat(firstHalfQuery, uint2str(_matchID), secondHalfQuery);
+    bytes32 queryId = oraclize_query("URL", query);
+    QueryIDtoMatchID[queryId] = _matchID;
+}
 
-  function __callback(bytes32 oracleID, string result){
+  function __callback(bytes32 _oracleID, string _result, bytes proof){
     if(msg.sender != oraclize_cbAddress()) revert();
-    jsonData = result;
-    emit PrintJson(result);
-  }
+    require(QueryIDtoMatchID[_oracleID] != 0);
+    uint Input = QueryIDtoMatchID[_oracleID];
+    InputToResult[Input] = _result;
+    matches[QueryIDtoMatchID[_oracleID]].winner = InputToResult[Input];
+}
 
-
-
-  function startMatch(string t1, string t2) onlyOwner {
-  matchCount++;
-  matches[matchCount] = Match(matchCount, testString, t1, t2, 0, 0, 0, 0, true, "none");
-  }
-
-
+  function startMatch(string t1, string t2, uint matchID) onlyOwner {
+  matches[matchID] = Match(matchID, t1, t2, 0, 0, 0, 0, true, "none");
+}
 
   function startBet(string _choice, uint _id) payable public {
 
-    require(msg.value >= 0.01 ether);
-    require( compareStrings(_choice, matches[_id].team1) == true || compareStrings(_choice, matches[_id].team2) == true);
-    require(matches[_id].id != 0);
-    require(matches[_id].betsOpen == true);
+  require(msg.value >= 0.01 ether);
+  require( compareStrings(_choice, matches[_id].team1) == true || compareStrings(_choice, matches[_id].team2) == true);
+  require(matches[_id].id != 0);
+  require(matches[_id].betsOpen == true);
 
 
-    uint256 _amount = msg.value;
-    uint256 _house = (_amount * 5) / 100;
+  uint256 _amount = msg.value;
+  uint256 _house = (_amount * 5) / 100;
 
-    _amount = _amount - _house;
-    matches[_id].house += _house;
+  _amount = _amount - _house;
+  matches[_id].house += _house;
 
-    string memory _teamPicked;
-    _teamPicked = "none";
+  string memory _teamPicked;
+  _teamPicked = "none";
 
-    if(compareStrings(_choice,matches[_id].team1) == true){
-      _teamPicked = matches[_id].team1;
-      matches[_id].t1_pool += _amount;
-    } else if(compareStrings(_choice,matches[_id].team2) == true){
-      _teamPicked = matches[_id].team2;
-      matches[_id].t2_pool += _amount;
-    }
-    matches[_id].betPool += _amount;
+  if(compareStrings(_choice,matches[_id].team1) == true){
+    _teamPicked = matches[_id].team1;
+    matches[_id].t1_pool += _amount;
+  } else if(compareStrings(_choice,matches[_id].team2) == true){
+    _teamPicked = matches[_id].team2;
+    matches[_id].t2_pool += _amount;
+  }
+  matches[_id].betPool += _amount;
 
-    bets[msg.sender] = Bet(
-      _id,
-      _teamPicked,
-      _amount
+  bets[msg.sender] = Bet(
+    _id,
+    _teamPicked,
+    _amount
     );
+}
 
+function compareStrings (string a, string b) view returns (bool){
+     return keccak256(a) == keccak256(b);
+}
+
+function endBetting(uint _matchID) onlyOwner {
+  require(matches[_matchID].id != 0);
+  require(matches[_matchID].betsOpen == true);
+  matches[_matchID].betsOpen = false;
+}
+
+/*function pickWinner(uint _matchID, string _winner) onlyOwner {
+  require( compareStrings(_winner, matches[_matchID].team1) == true || compareStrings(_winner, matches[_matchID].team2) == true);
+  require(matches[_matchID].id != 0);
+  require(matches[_matchID].betsOpen == false);
+  string memory winningTeam;
+  winningTeam = "none";
+  if(compareStrings(_winner, matches[_matchID].team1) == true) {
+    winningTeam = matches[_matchID].team1;
+    matches[_matchID].winner = winningTeam;
+  } else {
+    winningTeam = matches[_matchID].team2;
+    matches[_matchID].winner = winningTeam;
   }
+} */
+function pickWinner(uint _matchID, string _winner){
+require(matches[_matchID].id != 0);
+//require(matches[_matchID].betsOpen == false);
+if(msg.sender != oraclize_cbAddress()) revert();
+matches[_matchID].winner = _winner;
+}
 
-  function compareStrings (string a, string b) view returns (bool){
-       return keccak256(a) == keccak256(b);
-   }
 
-  function endBetting(uint _matchID) onlyOwner {
-    require(matches[_matchID].id != 0);
-    require(matches[_matchID].betsOpen == true);
-    matches[_matchID].betsOpen = false;
+
+function calculateResults(uint _matchID) {
+  //Require that the bettor hasnt called calculateResults anymore
+  require(!bettors[msg.sender]);
+
+  // Record that the bettor has called calculate Results
+  bettors[msg.sender] = true;
+
+  uint256 bettedAmount = bets[msg.sender].amount;
+  uint256 team1Odds = matches[_matchID].t2_pool / matches[_matchID].t1_pool;
+  uint256 team2Odds = matches[_matchID].t1_pool / matches[_matchID].t2_pool;
+  uint256 winningAmount = 0;
+
+  owner.transfer(matches[_matchID].house);
+  if(compareStrings(matches[_matchID].winner,matches[_matchID].team1) == true && compareStrings(bets[msg.sender].team, matches[_matchID].team1) == true){
+      winningAmount = team1Odds * bettedAmount + bettedAmount;
+      msg.sender.transfer(winningAmount);
+  } else if (compareStrings(matches[_matchID].winner,matches[_matchID].team2) == true && compareStrings(bets[msg.sender].team, matches[_matchID].team2) == true) {
+      winningAmount = team2Odds * bettedAmount + bettedAmount;
+      msg.sender.transfer(winningAmount);
   }
+}
 
-  function pickWinner(uint _matchID, string _winner) onlyOwner {
-    require( compareStrings(_winner, matches[_matchID].team1) == true || compareStrings(_winner, matches[_matchID].team2) == true);
-    require(matches[_matchID].id != 0);
-    require(matches[_matchID].betsOpen == false);
-    string memory winningTeam;
-    winningTeam = "none";
-    if(compareStrings(_winner, matches[_matchID].team1) == true) {
-      winningTeam = matches[_matchID].team1;
-      matches[_matchID].winner = winningTeam;
-    } else {
-      winningTeam = matches[_matchID].team2;
-      matches[_matchID].winner = winningTeam;
-    }
-
-
-
-  }
-
-
-  function calculateResults(uint _matchID) {
-    //Require that the bettor hasnt called calculateResults anymore
-    require(!bettors[msg.sender]);
-
-    // Record that the bettor has called calculate Results
-    bettors[msg.sender] = true;
-
-    uint256 bettedAmount = bets[msg.sender].amount;
-    uint256 team1Odds = matches[_matchID].t2_pool / matches[_matchID].t1_pool;
-    uint256 team2Odds = matches[_matchID].t1_pool / matches[_matchID].t2_pool;
-    uint256 winningAmount = 0;
-
-    owner.transfer(matches[_matchID].house);
-    if(compareStrings(matches[_matchID].winner,matches[_matchID].team1) == true && compareStrings(bets[msg.sender].team, matches[_matchID].team1) == true){
-        winningAmount = team1Odds * bettedAmount + bettedAmount;
-        msg.sender.transfer(winningAmount);
-    } else if (compareStrings(matches[_matchID].winner,matches[_matchID].team2) == true && compareStrings(bets[msg.sender].team, matches[_matchID].team2) == true) {
-        winningAmount = team2Odds * bettedAmount + bettedAmount;
-        msg.sender.transfer(winningAmount);
-    }
-  }
-
-  modifier onlyOwner {
-    require(owner == msg.sender);
-    _;
+modifier onlyOwner {
+  require(owner == msg.sender);
+  _;
   }
 }
